14032caa2d3dbe4526eb64160d2f9e8466569f2c
LuYomayel <luciano.yomayel@alumnos.frgp.utn.edu.ar>
2022-03-11 15:23:08 -0300

    Correccion clase 6

--- Clase 6/index.js
+++ Clase 6/index.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
-
+// const fs = require('fs');
 class Archivo {
     constructor(nombre){
         this.nombre = nombre;
@@ -9,14 +9,14 @@ class Archivo {
             if(!fs.existsSync(`${this.nombre}`)){
                 
                 await fs.promises.writeFile(`${this.nombre}`,'[]')
-                return console.log('El archivo no existía asi que lo creamos.')
+                return console.log('[]');
             }else{
                 
                 let content = await fs.promises.readFile(`${this.nombre}`, 'utf-8')
                 console.log(content);
             }
         } catch (error) {
-            console.log('Error al leer el archivo: ' + error);
+            console.log('Error al leer el archivo (leer): ' + error);
         }
         
     }
@@ -24,7 +24,12 @@ class Archivo {
 
         try {
             if(!fs.existsSync(`${this.nombre}`)) {
-                console.log('El archivo no existe');
+                await fs.promises.writeFile(`${this.nombre}`,'[]')
+                
+                producto.id = 1;
+                listaProductos = [...producto];
+                await fs.promises.writeFile(`${this.nombre}`, JSON.stringify(listaProductos,null,'\t'))
+                console.log('Cargamos el producto al array!')
             }else{
                 let listadoProductos = await fs.promises.readFile(`${this.nombre}`, 'utf-8')
                 let parse = JSON.parse(listadoProductos)
@@ -37,7 +42,7 @@ class Archivo {
 
 
         } catch (error) {
-            console.log('Error al leer el archivo: ' + error);
+            console.log('Error al leer el archivo (guardar): ' + error);
         }
     }
     async borrar(){
@@ -51,7 +56,7 @@ class Archivo {
                 return console.log('Documento Borrado')
             }
         } catch (error) {
-            
+            console.log('Error al leer el archivo (borrar): ' + error);
         }
 
         
@@ -68,7 +73,7 @@ class Producto{
 }
 
 let archivo = new Archivo('productos.txt');
-await archivo.leer();
+//await archivo.leer();
 
 
 let p1 = new Producto('Samsung', 65000, 'https://images.samsung.com/is/image/samsung/latin-ru7100-un55ru7100pxpa-frontblack-155445710?$684_547_PNG$')
@@ -76,8 +81,8 @@ let p2 = new Producto('Samsung', 65000, 'https://images.samsung.com/is/image/sam
 
 
 await archivo.guardar(p1);
-await archivo.leer();
-await archivo.borrar();
+//await archivo.leer();
+//await archivo.borrar();
 
 // No me va a dejar guardar porque el archivo no existe
-await archivo.guardar(p2);
+//await archivo.guardar(p2);
